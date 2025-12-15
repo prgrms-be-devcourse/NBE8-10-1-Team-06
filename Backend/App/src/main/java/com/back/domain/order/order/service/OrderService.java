5f840ccbb379ee5c23e1e2363f0166e6c7eaf8bf
AquilaXk <illusiveman7@gmail.com>
2025-12-15 17:42:49 +0900

    feat: implement read service logic

--- Backend/App/src/main/java/com/back/domain/order/order/service/OrderService.java
+++ Backend/App/src/main/java/com/back/domain/order/order/service/OrderService.java
@@ -1,4 +1,18 @@
 package com.back.domain.order.order.service;
 
+import com.back.domain.order.order.entity.OrderEntity;
+import com.back.domain.order.order.repository.OrderRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
 public class OrderService {
+    private final OrderRepository orderRepository;
+
+    public Optional<OrderEntity> findbyid(Long id){
+        return orderRepository.findById(id);
+    }
 }
