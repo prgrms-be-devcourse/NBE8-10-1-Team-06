bae30939c86eac66796693008395bb10606b56ac
AquilaXk <illusiveman7@gmail.com>
2025-12-15 17:23:43 +0900

    feat: Implement OrderRepository

--- Backend/App/src/main/java/com/back/domain/order/order/repository/OrderRepository.java
+++ Backend/App/src/main/java/com/back/domain/order/order/repository/OrderRepository.java
@@ -1,4 +1,7 @@
 package com.back.domain.order.order.repository;
 
-public class OrderRepository {
+import com.back.domain.order.order.entity.OrderEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface OrderRepository extends JpaRepository<OrderEntity, Long> {
 }
