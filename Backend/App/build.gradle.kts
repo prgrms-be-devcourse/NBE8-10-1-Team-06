8b96bd2fa17e3093473d5252b98283f6153205da
AquilaXk <illusiveman7@gmail.com>
2025-12-15 17:15:27 +0900

    feat: Add Spring JPA dependency

--- Backend/App/build.gradle.kts
+++ Backend/App/build.gradle.kts
@@ -32,6 +32,7 @@ dependencies {
 	annotationProcessor("org.projectlombok:lombok")
 	testImplementation("org.springframework.boot:spring-boot-starter-webmvc-test")
 	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
 }
 
 tasks.withType<Test> {
