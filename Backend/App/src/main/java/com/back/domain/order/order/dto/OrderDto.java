c6081c06d4bf8751deb3c9cf87bb1995c163036d
hhyukk <hhyukk1273@gmail.com>
2025-12-16 11:29:24 +0900

    중간점검

--- Backend/App/src/main/java/com/back/domain/order/order/dto/OrderDto.java
+++ Backend/App/src/main/java/com/back/domain/order/order/dto/OrderDto.java
@@ -1,4 +1,13 @@
 package com.back.domain.order.order.dto;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
 public class OrderDto {
+
+    public record CreateRequest(
+            String email,
+            String address,
+            Integer postcode
+    ) {}
 }
