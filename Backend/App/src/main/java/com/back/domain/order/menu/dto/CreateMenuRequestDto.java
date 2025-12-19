ac7e60d65428d1e337cf8e4baf968423a809d95a
hhyukk <hhyukk1273@gmail.com>
2025-12-19 11:40:46 +0900

    fix: CreateMenuRequestDto에 @Getter 어노테이션 추가 MenuController에서 getPrice() 메서드 호출 오류 수정

--- Backend/App/src/main/java/com/back/domain/order/menu/dto/CreateMenuRequestDto.java
+++ Backend/App/src/main/java/com/back/domain/order/menu/dto/CreateMenuRequestDto.java
@@ -5,6 +5,7 @@ import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import lombok.Getter;
 
+@Getter
 public class CreateMenuRequestDto {
     @NotBlank(message = "이메일을 입력하세요.")
     @Email(message = "유효하지 않은 이메일 형식입니다.")
